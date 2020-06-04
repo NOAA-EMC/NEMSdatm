b48e8391b69c83265417814c7ebc8d6c1dbd8669
denise.worthen <Denise.Worthen@noaa.gov>
2020-06-04 11:33:28 +0000

    add abort of datm forcing file is not found

--- DATM/AtmForce.F90
+++ DATM/AtmForce.F90
@@ -19,11 +19,15 @@ subroutine AtmForce(gcomp,exportState,externalClock,initmode,rc)
   type(ESMF_Time)               :: currTime
   type(ESMF_Time)               :: nextTime
   type(ESMF_TimeInterval)       :: timeStep
+  type(ESMF_VM)                 :: vm
 
   integer(kind=ESMF_KIND_I4)    :: year, month, day, hour, jday
 
+  integer :: mytask
   integer :: ii,nfields
   integer :: iii, iid, iiu
+  logical :: fexists 
+  integer :: fpresent(1)
 
   character(len=ESMF_MAXSTR) :: varname
   character(len=ESMF_MAXSTR) :: filename 
@@ -44,6 +48,12 @@ subroutine AtmForce(gcomp,exportState,externalClock,initmode,rc)
   rc = ESMF_SUCCESS
 
   call ESMF_LogWrite("User routine AtmForce started", ESMF_LOGMSG_INFO)
+
+  call ESMF_GridCompGet(gcomp, vm=vm, rc=rc)
+  if (ChkErr(rc,__LINE__,u_FILE_u)) return
+
+  call ESMF_VMGet(vm, localPet=mytask, rc=rc)
+  if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
   ! at initialization, get the current forecast hour file, not the forward 
   ! forecast hour 
@@ -76,35 +86,48 @@ subroutine AtmForce(gcomp,exportState,externalClock,initmode,rc)
    
    call ESMF_TimeGet(nextTime,h_r8=hfwd,rc=rc)
   endif
-  write(msgString,'(3a,f12.3)')'using ',trim(filename),' at fwd clock hour ',real(hfwd,4)
+  write(msgString,'(3a,f12.3)')'using ',trim(filename),' at fwd clock hour ',hfwd
   call ESMF_LogWrite(trim(msgString), ESMF_LOGMSG_INFO)
 
-  ! read the Atm field data into the Fwd bundle
-  nfields = size(AtmBundleFields)
-  do ii = 1,nfields
-   if(AtmBundleFields(ii)%isPresent)then
-    varname = trim(AtmBundleFields(ii)%file_varname)
+  fexists = .false.
+  if(mytask == 0)inquire(file=trim(filename), exist=fexists)
+  if(fexists) fpresent(1) = 1
+  call ESMF_VMBroadCast(vm, fpresent, count=1, rootPet=0, rc=rc)
+  if (ChkErr(rc,__LINE__,u_FILE_u)) return
+  if(fpresent(1) == 1)fexists = .true.
 
-    ! get the '_fwd' field
-    call ESMF_FieldBundleGet(AtmBundleFwd, &
-                             fieldName=trim(AtmBundleFields(ii)%field_name)//'_fwd', &
-                             field=field, rc=rc)
-    if (ChkErr(rc,__LINE__,u_FILE_u)) return
+  if(.not. fexists)then
+    if(mytask == 0)print *,'Cannot read file '//trim(filename)//': Aborting'
+    call ESMF_LogWrite('Cannot read file '//trim(filename)//': Aborting', ESMF_LOGMSG_INFO)
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  else
+    ! read the Atm field data into the Fwd bundle
+    nfields = size(AtmBundleFields)
+    do ii = 1,nfields
+     if(AtmBundleFields(ii)%isPresent)then
+      varname = trim(AtmBundleFields(ii)%file_varname)
 
-    call ESMF_FieldRead(field, &
-                        fileName=trim(filename), &
-                        variableName = trim(varname), &
-                        rc=rc)
-    if (ChkErr(rc,__LINE__,u_FILE_u)) return
+      ! get the '_fwd' field
+      call ESMF_FieldBundleGet(AtmBundleFwd, &
+                               fieldName=trim(AtmBundleFields(ii)%field_name)//'_fwd', &
+                               field=field, rc=rc)
+      if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
-    call ESMF_FieldGet(field,farrayPtr=AtmBundleFields(ii)%farrayPtr_fwd,rc=rc)
-    if (ChkErr(rc,__LINE__,u_FILE_u)) return
+      call ESMF_FieldRead(field, &
+                          fileName=trim(filename), &
+                          variableName = trim(varname), &
+                          rc=rc)
+      if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
-     write(msgString,'(i6,2a18,f14.5)')ii,' inside AtmForce  ',trim(varname), &
-                             AtmBundleFields(ii)%farrayPtr_fwd(iprnt,jprnt)
-     call ESMF_LogWrite(trim(msgString), ESMF_LOGMSG_INFO)
-   endif !isPresent
-  enddo
+      call ESMF_FieldGet(field,farrayPtr=AtmBundleFields(ii)%farrayPtr_fwd,rc=rc)
+      if (ChkErr(rc,__LINE__,u_FILE_u)) return
+
+       write(msgString,'(i6,2a18,f14.5)')ii,' inside AtmForce  ',trim(varname), &
+                               AtmBundleFields(ii)%farrayPtr_fwd(iprnt,jprnt)
+       call ESMF_LogWrite(trim(msgString), ESMF_LOGMSG_INFO)
+     endif !isPresent
+    enddo
+  end if
 
   ! Check for fields which are not Present but are needed
   ! Not very clean---would have been better to create this field in the forcing file
